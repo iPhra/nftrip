8f516168f202d8aa74d9943357471e756c83a69a
iPhra <extreme_enigma@hotmail.it>
2021-12-13 18:29:16 +0100

    change default to adam

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -255,7 +255,7 @@ if __name__ == "__main__":
     parser.add_argument("--style_weight", type=float, help="weight factor for style loss", default=6e4)
     parser.add_argument("--tv_weight", type=float, help="weight factor for total variation loss", default=1e0)
 
-    parser.add_argument("--optimizer", type=str, choices=['lbfgs', 'adam'], default='lbfgs')
+    parser.add_argument("--optimizer", type=str, choices=['lbfgs', 'adam'], default='adam')
     parser.add_argument("--model", type=str, choices=['vgg16', 'vgg19'], default='vgg19')
     parser.add_argument("--init_method", type=str, choices=['random', 'content', 'style'], default='content')
     parser.add_argument("--saving_freq", type=int, help="saving frequency for intermediate images (-1 means only final)", default=-1)
