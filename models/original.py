6d2c6ea7961651df60972ab5a206db726f932d44
iPhra <extreme_enigma@hotmail.it>
2022-01-21 15:02:43 +0100

    create OOP structure

---
+++ models/original.py
@@ -0,0 +1,361 @@
+import logging
+import os
+
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch.autograd import Variable
+from torch.optim import LBFGS, Adam
+from torchvision import transforms
+
+from models.definitions.vgg_nets import Vgg16, Vgg16Experimental, Vgg19
+
+logger = logging.getLogger(__name__)
+
+IMAGENET_MEAN_255 = [123.675, 116.28, 103.53]
+IMAGENET_STD_NEUTRAL = [1, 1, 1]
+ITERATIONS_DICT = {
+    "lbfgs": 1000,
+    "adam": 1000,
+}
+
+
+class Original:
+    def __init__(self, config):
+        self.config = config
+        self.content_img_path = config["content_images_dir"] / config["content_img_name"]
+        self.style_img_path = config["style_images_dir"] / config["style_img_name"]
+        self.optimizer = config['optimizer']
+        self.height = config['height']
+        self.iterations = ITERATIONS_DICT[self.optimizer]
+    
+    def load_image(self, img_path, target_shape=None):
+        if not os.path.exists(img_path):
+            raise Exception(f'Path does not exist: {img_path}')
+        img = cv.imread(img_path)[:, :, ::-1]  # [:, :, ::-1] converts BGR (opencv format...) into RGB
+
+        if target_shape is not None:  # resize section
+            if isinstance(target_shape, int) and target_shape != -1:  # scalar -> implicitly setting the height
+                current_height, current_width = img.shape[:2]
+                new_height = target_shape
+                new_width = int(current_width * (new_height / current_height))
+                img = cv.resize(img, (new_width, new_height), interpolation=cv.INTER_CUBIC)
+            else:  # set both dimensions to target shape
+                img = cv.resize(img, (target_shape[1], target_shape[0]), interpolation=cv.INTER_CUBIC)
+
+        # this need to go after resizing - otherwise cv.resize will push values outside of [0,1] range
+        img = img.astype(np.float32)  # convert from uint8 to float32
+        img /= 255.0  # get to [0, 1] range
+        return img
+
+    def prepare_img(self, img_path, target_shape, device):
+        img = self.load_image(img_path, target_shape=target_shape)
+
+        # normalize using ImageNet's mean
+        # [0, 255] range worked much better for me than [0, 1] range (even though PyTorch models were trained on latter)
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.mul(255)),
+            transforms.Normalize(mean=IMAGENET_MEAN_255, std=IMAGENET_STD_NEUTRAL)
+        ])
+
+        img = transform(img).to(device).unsqueeze(0)
+
+        return img
+
+    @staticmethod
+    def save_image(img, img_path):
+        if len(img.shape) == 2:
+            img = np.stack((img,) * 3, axis=-1)
+        cv.imwrite(img_path, img[:, :, ::-1])  # [:, :, ::-1] converts rgb into bgr (opencv contraint...)
+
+    def generate_out_img_name(self):
+        prefix = os.path.basename(self.config['content_img_name']).split('.')[0] + '_' + os.path.basename(self.config['style_img_name']).split('.')[0]
+        # called from the reconstruction script
+        if 'reconstruct_script' in self.config:
+            suffix = f'_o_{self.optimizer}_h_{str(self.height)}_m_{self.config["model"]}{self.onfig["img_format"][1]}'
+        else:
+            suffix = f'_o_{self.optimizer}_i_{self.config["init_method"]}_h_{str(self.height)}_m_{self.config["model"]}_cw_{self.config["content_weight"]}_sw_{self.config["style_weight"]}_tv_{self.config["tv_weight"]}{self.config["img_format"][1]}'
+        return prefix + suffix
+
+
+    def save_and_maybe_display(self, optimizing_img, dump_path, img_id, num_of_iterations, should_display=False):
+        saving_freq = self.config['saving_freq']
+        out_img = optimizing_img.squeeze(axis=0).to('cpu').detach().numpy()
+        out_img = np.moveaxis(out_img, 0, 2)  # swap channel from 1st to 3rd position: ch, _, _ -> _, _, chr
+
+        # for saving_freq == -1 save only the final result (otherwise save with frequency saving_freq and save the last pic)
+        if img_id == num_of_iterations-1 or (saving_freq > 0 and img_id % saving_freq == 0) or ((saving_freq > 0) and (img_id<20)):
+            img_format = self.config['img_format']
+            out_img_name = str(img_id).zfill(img_format[0]) + img_format[1] if saving_freq != -1 else self.generate_out_img_name()
+            dump_img = np.copy(out_img)
+            dump_img += np.array(IMAGENET_MEAN_255).reshape((1, 1, 3))
+            dump_img = np.clip(dump_img, 0, 255).astype('uint8')
+            cv.imwrite(os.path.join(dump_path, out_img_name), dump_img[:, :, ::-1])
+
+        if should_display:
+            plt.imshow(np.uint8(self.get_uint8_range(out_img)))
+            plt.show()
+
+    @staticmethod
+    def get_uint8_range(x):
+        if isinstance(x, np.ndarray):
+            x -= np.min(x)
+            x /= np.max(x)
+            x *= 255
+            return x
+        else:
+            raise ValueError(f'Expected numpy array got {type(x)}')
+
+    @staticmethod
+    def prepare_model(model, device):
+        # we are not tuning model weights -> we are only tuning optimizing_img's pixels! (that's why requires_grad=False)
+        experimental = False
+        if model == 'vgg16':
+            if experimental:
+                # much more flexible for experimenting with different style representations
+                model = Vgg16Experimental(requires_grad=False, show_progress=True)
+            else:
+                model = Vgg16(requires_grad=False, show_progress=True)
+        elif model == 'vgg19':
+            model = Vgg19(requires_grad=False, show_progress=True)
+        else:
+            raise ValueError(f'{model} not supported.')
+
+        content_feature_maps_index = model.content_feature_maps_index
+        style_feature_maps_indices = model.style_feature_maps_indices
+        layer_names = model.layer_names
+
+        content_fms_index_name = (content_feature_maps_index, layer_names[content_feature_maps_index])
+        style_fms_indices_names = (style_feature_maps_indices, layer_names)
+        return model.to(device).eval(), content_fms_index_name, style_fms_indices_names
+
+    @staticmethod
+    def gram_matrix(x, should_normalize=True):
+        (b, ch, h, w) = x.size()
+        features = x.view(b, ch, w * h)
+        features_t = features.transpose(1, 2)
+        gram = features.bmm(features_t)
+        if should_normalize:
+            gram /= ch * h * w
+        return gram
+
+    @staticmethod
+    def total_variation(y):
+        return torch.sum(torch.abs(y[:, :, :, :-1] - y[:, :, :, 1:])) + \
+            torch.sum(torch.abs(y[:, :, :-1, :] - y[:, :, 1:, :]))
+
+    def build_loss(
+        self,
+        neural_net,
+        optimizing_img,
+        target_representations,
+        content_feature_maps_index,
+        style_feature_maps_indices,
+    ):
+        target_content_representation = target_representations[0]
+        target_style_representation = target_representations[1]
+
+        current_set_of_feature_maps = neural_net(optimizing_img)
+
+        current_content_representation = current_set_of_feature_maps[
+            content_feature_maps_index
+        ].squeeze(axis=0)
+        content_loss = torch.nn.MSELoss(reduction="mean")(
+            target_content_representation, current_content_representation
+        )
+
+        style_loss = 0.0
+        current_style_representation = [
+            self.gram_matrix(x)
+            for cnt, x in enumerate(current_set_of_feature_maps)
+            if cnt in style_feature_maps_indices
+        ]
+        for gram_gt, gram_hat in zip(
+            target_style_representation, current_style_representation
+        ):
+            style_loss += torch.nn.MSELoss(reduction="sum")(gram_gt[0], gram_hat[0])
+        style_loss /= len(target_style_representation)
+
+        tv_loss = self.total_variation(optimizing_img)
+
+        total_loss = (
+            self.config["content_weight"] * content_loss
+            + self.config["style_weight"] * style_loss
+            + self.config["tv_weight"] * tv_loss
+        )
+
+        return total_loss, content_loss, style_loss, tv_loss
+
+
+    def make_tuning_step(
+        self,
+        neural_net,
+        optimizer,
+        target_representations,
+        content_feature_maps_index,
+        style_feature_maps_indices,
+    ):
+        # Builds function that performs a step in the tuning loop
+        def tuning_step(optimizing_img):
+            total_loss, content_loss, style_loss, tv_loss = self.build_loss(
+                neural_net,
+                optimizing_img,
+                target_representations,
+                content_feature_maps_index,
+                style_feature_maps_indices,
+            )
+            # Computes gradients
+            total_loss.backward()
+            # Updates parameters and zeroes gradients
+            optimizer.step()
+            optimizer.zero_grad()
+            return total_loss, content_loss, style_loss, tv_loss
+
+        # Returns the function that will be called inside the tuning loop
+        return tuning_step
+
+
+    def predict(self):
+        logger.info("Starting prediction")
+
+        out_dir_name = (
+            "combined_"
+            + os.path.split(self.content_img_path)[1].split(".")[0]
+            + "_"
+            + os.path.split(self.style_img_path)[1].split(".")[0]
+        )
+        dump_path = self.config["output_img_dir"] / out_dir_name
+        dump_path.mkdir(exist_ok=True, parents=True)
+
+        # remove all files in the folder
+        [f.unlink() for f in dump_path.glob("*") if f.is_file()]
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        logger.debug(f"Using device {device} for training")
+
+        content_img = self.prepare_img(str(self.content_img_path), self.height, device)
+        style_img = self.prepare_img(str(self.style_img_path), self.height, device)
+
+        if self.config["init_method"] == "random":
+            # white_noise_img = np.random.uniform(-90., 90., content_img.shape).astype(np.float32)
+            gaussian_noise_img = np.random.normal(
+                loc=0, scale=90.0, size=content_img.shape
+            ).astype(np.float32)
+            init_img = torch.from_numpy(gaussian_noise_img).float().to(device)
+        elif self.config["init_method"] == "content":
+            init_img = content_img
+        else:
+            # init image has same dimension as content image - this is a hard constraint
+            # feature maps need to be of same size for content image and init image
+            style_img_resized = self.prepare_img(
+                self.style_img_path, np.asarray(content_img.shape[2:]), device
+            )
+            init_img = style_img_resized
+
+        # we are tuning optimizing_img's pixels! (that's why requires_grad=True)
+        optimizing_img = Variable(init_img, requires_grad=True)
+
+        (
+            neural_net,
+            content_feature_maps_index_name,
+            style_feature_maps_indices_names,
+        ) = self.prepare_model(self.config["model"], device)
+        logger.debug(f'Using {self.config["model"]} in the optimization procedure')
+
+        content_img_set_of_feature_maps = neural_net(content_img)
+        style_img_set_of_feature_maps = neural_net(style_img)
+
+        target_content_representation = content_img_set_of_feature_maps[
+            content_feature_maps_index_name[0]
+        ].squeeze(axis=0)
+        target_style_representation = [
+            self.gram_matrix(x)
+            for cnt, x in enumerate(style_img_set_of_feature_maps)
+            if cnt in style_feature_maps_indices_names[0]
+        ]
+        target_representations = [
+            target_content_representation,
+            target_style_representation,
+        ]
+
+        # Start of optimization procedure
+        unsuccessful = False
+        if self.config["optimizer"] == "adam":
+            logger.info("Using ADAM")
+            try:
+                optimizer = Adam((optimizing_img,), lr=1e1)
+                tuning_step = self.make_tuning_step(
+                    neural_net,
+                    optimizer,
+                    target_representations,
+                    content_feature_maps_index_name[0],
+                    style_feature_maps_indices_names[0],
+                )
+
+                for cnt in range(self.ITERATIONS[self.config["optimizer"]]):
+                    _ = tuning_step(optimizing_img)
+                    with torch.no_grad():
+                        self.save_and_maybe_display(
+                            optimizing_img,
+                            dump_path,
+                            self.config,
+                            cnt,
+                            self.ITERATIONS[["optimizer"]],
+                            should_display=False,
+                        )
+            except:
+                unsuccessful = True
+
+        elif self.optimizer == "lbfgs":
+            logger.info("Using LBFGS")
+            try:
+                # line_search_fn does not seem to have significant impact on result
+                optimizer = LBFGS(
+                    (optimizing_img,),
+                    max_iter=self.iterations,
+                    line_search_fn="strong_wolfe",
+                )
+                cnt = 0
+
+                def closure():
+                    nonlocal cnt
+                    if torch.is_grad_enabled():
+                        optimizer.zero_grad()
+                    total_loss, _, _, _ = self.build_loss(
+                        neural_net,
+                        optimizing_img,
+                        target_representations,
+                        content_feature_maps_index_name[0],
+                        style_feature_maps_indices_names[0],
+                    )
+                    if total_loss.requires_grad:
+                        total_loss.backward()
+                    with torch.no_grad():
+                        # logger.debug(f'L-BFGS | iteration: {cnt:03}, total loss={total_loss.item():12.4f}, content_loss={config["content_weight"] * content_loss.item():12.4f}, style loss={config["style_weight"] * style_loss.item():12.4f}, tv loss={config["tv_weight"] * tv_loss.item():12.4f}')
+                        self.save_and_maybe_display(
+                            optimizing_img,
+                            dump_path,
+                            self.config,
+                            cnt,
+                            self.iterations,
+                            should_display=False,
+                        )
+
+                    cnt += 1
+                    return total_loss
+
+                optimizer.step(closure)
+            except:
+                unsuccessful = True
+
+        logger.debug(f"Executed {cnt} iterations")
+        if ((cnt < self.iterations / 10) or unsuccessful == True) & (
+            self.optimizer == "lbfgs"
+        ):
+            self.optimizer = "adam"
+            return self.predict()
+
+        logger.info("Training finished")
+        return dump_path
--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -1,18 +1,15 @@
 import argparse
 import logging
 import os
+import shutil
 from datetime import datetime
 from pathlib import Path
 from time import time
 
-import numpy as np
-import torch
+import imageio
+from PIL import Image
 
-from torch.autograd import Variable
-from torch.optim import LBFGS, Adam
-
-import utils.utils as utils
-from utils.gifs import make_gif
+from models.original import Original
 from utils.upscale import ESRGANUpscale as Upscaler
 
 logs_folder = Path("logs")
@@ -28,234 +25,52 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def build_loss(
-    neural_net,
-    optimizing_img,
-    target_representations,
-    content_feature_maps_index,
-    style_feature_maps_indices,
-    config,
-):
-    target_content_representation = target_representations[0]
-    target_style_representation = target_representations[1]
+def copy_output(optimization_config, results_path):
+    optimization_config['images_path'].mkdir(exist_ok=True, parents=True)
+    optimization_config['gifs_path'].mkdir(exist_ok=True, parents=True)
+    destination_filename = optimization_config['output_img_name'].split('.')[0]
 
-    current_set_of_feature_maps = neural_net(optimizing_img)
+    source_img_filename = sorted(list(results_path.glob('*.png')))[-1]
+    logger.info(f'Copying {source_img_filename}')
+    destination_img_file = optimization_config['images_path'] / (
+        destination_filename + '.png')
+    shutil.copy(source_img_filename, destination_img_file)
 
-    current_content_representation = current_set_of_feature_maps[
-        content_feature_maps_index
-    ].squeeze(axis=0)
-    content_loss = torch.nn.MSELoss(reduction="mean")(
-        target_content_representation, current_content_representation
-    )
+    if optimization_config['gif']:
+        source_gif_filename = list(results_path.glob('*.gif'))[0]
+        logger.info(f'Copying {source_gif_filename}')
+        destination_video_file = optimization_config['gifs_path'] / (
+            'g' + destination_filename + '.gif')
+        logger.info(f'Copying to {destination_video_file}')
+        shutil.copy(source_gif_filename, destination_video_file)
 
-    style_loss = 0.0
-    current_style_representation = [
-        utils.gram_matrix(x)
-        for cnt, x in enumerate(current_set_of_feature_maps)
-        if cnt in style_feature_maps_indices
-    ]
-    for gram_gt, gram_hat in zip(
-        target_style_representation, current_style_representation
-    ):
-        style_loss += torch.nn.MSELoss(reduction="sum")(gram_gt[0], gram_hat[0])
-    style_loss /= len(target_style_representation)
-
-    tv_loss = utils.total_variation(optimizing_img)
-
-    total_loss = (
-        config["content_weight"] * content_loss
-        + config["style_weight"] * style_loss
-        + config["tv_weight"] * tv_loss
-    )
-
-    return total_loss, content_loss, style_loss, tv_loss
+    shutil.rmtree(results_path, ignore_errors=True)
 
 
-def make_tuning_step(
-    neural_net,
-    optimizer,
-    target_representations,
-    content_feature_maps_index,
-    style_feature_maps_indices,
-    config,
-):
-    # Builds function that performs a step in the tuning loop
-    def tuning_step(optimizing_img):
-        total_loss, content_loss, style_loss, tv_loss = build_loss(
-            neural_net,
-            optimizing_img,
-            target_representations,
-            content_feature_maps_index,
-            style_feature_maps_indices,
-            config,
-        )
-        # Computes gradients
-        total_loss.backward()
-        # Updates parameters and zeroes gradients
-        optimizer.step()
-        optimizer.zero_grad()
-        return total_loss, content_loss, style_loss, tv_loss
+def make_gif(config, results_path):
+    logger.info('Creating gif..')
 
-    # Returns the function that will be called inside the tuning loop
-    return tuning_step
+    results = list(sorted(results_path.glob('*.png')))[-1]
+    transf = Image.open(results)
 
+    content_img_path = config['content_images_dir'] / \
+        config['content_img_name']
+    orig = Image.open(content_img_path)
+    orig = orig.resize((transf.width, transf.height), Image.ANTIALIAS)
 
-def neural_style_transfer(config):
-    logger.info("Starting training..")
+    images = []
+    for i in range(0, 255, 15):
+        orig_new = orig.copy()
+        orig_new.putalpha(255-i)
 
-    content_img_path = config["content_images_dir"] / config["content_img_name"]
-    style_img_path = config["style_images_dir"] / config["style_img_name"]
+        transf_new = transf.copy()
+        transf_new.putalpha(i)
 
-    out_dir_name = (
-        "combined_"
-        + os.path.split(content_img_path)[1].split(".")[0]
-        + "_"
-        + os.path.split(style_img_path)[1].split(".")[0]
-    )
-    dump_path = config["output_img_dir"] / out_dir_name
-    dump_path.mkdir(exist_ok=True, parents=True)
-    # remove all files in the folder
-    [f.unlink() for f in dump_path.glob("*") if f.is_file()]
+        new = Image.alpha_composite(orig_new, transf_new)
+        images.append(new)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    logger.debug(f"Using device {device} for training")
-
-    content_img = utils.prepare_img(str(content_img_path), config["height"], device)
-    style_img = utils.prepare_img(str(style_img_path), config["height"], device)
-
-    if config["init_method"] == "random":
-        # white_noise_img = np.random.uniform(-90., 90., content_img.shape).astype(np.float32)
-        gaussian_noise_img = np.random.normal(
-            loc=0, scale=90.0, size=content_img.shape
-        ).astype(np.float32)
-        init_img = torch.from_numpy(gaussian_noise_img).float().to(device)
-    elif config["init_method"] == "content":
-        init_img = content_img
-    else:
-        # init image has same dimension as content image - this is a hard constraint
-        # feature maps need to be of same size for content image and init image
-        style_img_resized = utils.prepare_img(
-            style_img_path, np.asarray(content_img.shape[2:]), device
-        )
-        init_img = style_img_resized
-
-    # we are tuning optimizing_img's pixels! (that's why requires_grad=True)
-    optimizing_img = Variable(init_img, requires_grad=True)
-
-    (
-        neural_net,
-        content_feature_maps_index_name,
-        style_feature_maps_indices_names,
-    ) = utils.prepare_model(config["model"], device)
-    logger.debug(f'Using {config["model"]} in the optimization procedure')
-
-    content_img_set_of_feature_maps = neural_net(content_img)
-    style_img_set_of_feature_maps = neural_net(style_img)
-
-    target_content_representation = content_img_set_of_feature_maps[
-        content_feature_maps_index_name[0]
-    ].squeeze(axis=0)
-    target_style_representation = [
-        utils.gram_matrix(x)
-        for cnt, x in enumerate(style_img_set_of_feature_maps)
-        if cnt in style_feature_maps_indices_names[0]
-    ]
-    target_representations = [
-        target_content_representation,
-        target_style_representation,
-    ]
-
-    # magic numbers in general are a big no no - some things in this code are left like this by design to avoid clutter
-    num_of_iterations = {
-        "lbfgs": 1000,
-        "adam": 1000,
-    }
-
-    #
-    # Start of optimization procedure
-    #
-    unsuccessful = False
-    if config["optimizer"] == "adam":
-        logger.info("Using ADAM")
-        try:
-            optimizer = Adam((optimizing_img,), lr=1e1)
-            tuning_step = make_tuning_step(
-                neural_net,
-                optimizer,
-                target_representations,
-                content_feature_maps_index_name[0],
-                style_feature_maps_indices_names[0],
-                config,
-            )
-            for cnt in range(num_of_iterations[config["optimizer"]]):
-                total_loss, content_loss, style_loss, tv_loss = tuning_step(
-                    optimizing_img
-                )
-                with torch.no_grad():
-                    # logger.debug(f'Adam | iteration: {cnt:03}, total loss={total_loss.item():12.4f}, content_loss={config["content_weight"] * content_loss.item():12.4f}, style loss={config["style_weight"] * style_loss.item():12.4f}, tv loss={config["tv_weight"] * tv_loss.item():12.4f}')
-                    utils.save_and_maybe_display(
-                        optimizing_img,
-                        dump_path,
-                        config,
-                        cnt,
-                        num_of_iterations[config["optimizer"]],
-                        should_display=False,
-                    )
-        except:
-            unsuccessful = True
-
-    elif config["optimizer"] == "lbfgs":
-        logger.info("Using LBFGS")
-        try:
-            # line_search_fn does not seem to have significant impact on result
-            optimizer = LBFGS(
-                (optimizing_img,),
-                max_iter=num_of_iterations["lbfgs"],
-                line_search_fn="strong_wolfe",
-            )
-            cnt = 0
-
-            def closure():
-                nonlocal cnt
-                if torch.is_grad_enabled():
-                    optimizer.zero_grad()
-                total_loss, content_loss, style_loss, tv_loss = build_loss(
-                    neural_net,
-                    optimizing_img,
-                    target_representations,
-                    content_feature_maps_index_name[0],
-                    style_feature_maps_indices_names[0],
-                    config,
-                )
-                if total_loss.requires_grad:
-                    total_loss.backward()
-                with torch.no_grad():
-                    # logger.debug(f'L-BFGS | iteration: {cnt:03}, total loss={total_loss.item():12.4f}, content_loss={config["content_weight"] * content_loss.item():12.4f}, style loss={config["style_weight"] * style_loss.item():12.4f}, tv loss={config["tv_weight"] * tv_loss.item():12.4f}')
-                    utils.save_and_maybe_display(
-                        optimizing_img,
-                        dump_path,
-                        config,
-                        cnt,
-                        num_of_iterations[config["optimizer"]],
-                        should_display=False,
-                    )
-
-                cnt += 1
-                return total_loss
-
-            optimizer.step(closure)
-        except:
-            unsuccessful = True
-
-    logger.debug(f"Executed {cnt} iterations")
-    if ((cnt < num_of_iterations["lbfgs"] / 10) or unsuccessful == True) & (
-        config["optimizer"] == "lbfgs"
-    ):
-        config["optimizer"] = "adam"
-        return neural_style_transfer(config)
-
-    logger.info("Training finished")
-    return dump_path
+    images = images + images[::-1]
+    imageio.mimsave(f'{results_path}/out.gif', images, duration=0.01)
 
 
 def main(optimization_config):
@@ -281,8 +96,11 @@ def main(optimization_config):
 
     logger.debug(optimization_config)
 
-    # original NST (Neural Style Transfer) algorithm (Gatys et al.)
-    images_path = neural_style_transfer(optimization_config)
+    if optimization_config['model'] == 'original':
+        model = Original(optimization_config)
+        images_path = model.predict()
+    else:
+        raise ValueError("Invalid algorithm provided")
 
     if optimization_config["gif"]:
         make_gif(optimization_config, images_path)
@@ -292,7 +110,7 @@ def main(optimization_config):
         upscaler(images_path)
 
     # copy results to the respective folder
-    utils.copy_output(optimization_config, images_path)
+    copy_output(optimization_config, images_path)
 
     logger.info(f"Time elapsed: {time()-start}")
 
@@ -337,6 +155,9 @@ if __name__ == "__main__":
         type=float,
         help="weight factor for total variation loss",
         default=1e0,
+    )
+    parser.add_argument(
+        "--model", type=int, choices=["original"], help="Neural Style Transfer model", default="original"
     )
 
     parser.add_argument(
